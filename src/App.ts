20c0eef0f9500682e6d24e33eb1626ea38a83a22
Ryan Park <me@lionah.com>
2025-06-15 14:06:13 -0700

    Commit basic configuration for Node/TS (#1)

---
+++ src/App.ts
@@ -0,0 +1,7 @@
+import express from 'express';express
+
+const app = express();
+
+app.get('/', (req, res) => res.send('Hello World'));
+
+app.listen(3000, () => console.log('Server running on port 3000'))
