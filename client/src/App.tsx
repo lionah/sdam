40feefa909012deb67343c669fe971a8743bef7c
Ryan Park <me@lionah.com>
2025-07-03 22:33:40 -0700

    Redirect / to /assets for now (#11)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,13 +3,14 @@ import AssetsPage from "./pages/AssetsPage";
 import AssetsUploadPage from "./pages/AssetsUploadPage";
 import ErrorPage from "./pages/ErrorPage";
 import LayoutPage from "./pages/LayoutPage";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LayoutPage />}>
+          <Route index element={<Navigate to="/assets" replace />} />
           <Route path="/assets" element={<AssetsPage />} />
           <Route path="/assets/upload" element={<AssetsUploadPage />} />
           <Route path="*" element={<ErrorPage code="404" title="Page Not Found" />} />
